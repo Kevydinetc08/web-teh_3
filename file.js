cdf5c125682d7f343c018a08d91b4045d327e435
Vadim <vadikk87@mail.ru>
2023-09-18 03:14:31 +1200

    Добавил файл с описанием задания

---
+++ file.js
@@ -0,0 +1,8 @@
+function Grettings() {
+    let name = prompt('Назови мне имя и будет тебе счастье!');
+    msg = `"Ну здравствуй, ....... Брат!", ${name}!!`
+    alert(msg)
+};
+
+Grettings();
+console.log(msg);
